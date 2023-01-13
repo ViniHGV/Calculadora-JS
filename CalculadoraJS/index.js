e93c371084201f85e6bf5f3ef9b332ec0d8b6e5c
Vini <ixxvinicius@gmail.com>
2023-01-13 11:00:08 -0300

    Adicionando a tecla espaço e C

--- CalculadoraJS/index.js
+++ CalculadoraJS/index.js
@@ -5,6 +5,17 @@ const resultInput = document.getElementById('result')
 
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
+document.querySelectorAll('.charKey').forEach(function (charKeyBtn){
+    charKeyBtn.addEventListener('click', function (){
+        const value = charKeyBtn.dataset.value
+        input.value += value
+    })
+})
+document.getElementById('clear').addEventListener('click', function(){
+    input.value = ''
+    input.focus()
+})
+
 input.addEventListener('keydown', function(ev) {
     ev.preventDefault()
     if (allowedKeys.includes(ev.key)) {
@@ -14,7 +25,16 @@ input.addEventListener('keydown', function(ev) {
     if(ev.key ==='Backspace') {
         input.value = input.value.slice(0, -1)
     }
+    if(ev.key ==='Enter'){
+        calculate()
+    }
 })
+
+document.getElementById('equal').addEventListener('click', calculate)
+
+function calculate(){
+    
+}
 
 
 
